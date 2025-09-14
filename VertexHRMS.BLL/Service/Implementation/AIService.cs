21b5f4bc17548e3b7dcc906c0f7371785e503e6f
Abdallah Qusit <abdallah2512ahmed@gmail.com>
2025-09-14 23:48:00 +0300

    [Fet] Created AI Page Done

---
+++ VertexHRMS.BLL/Service/Implementation/AIService.cs
@@ -0,0 +1,27 @@
+﻿using VertexHRMS.BLL.ModelVM.AI;
+using VertexHRMS.BLL.Services.Abstraction;
+using VertexHRMS.DAL.Repo.Abstraction;
+namespace VertexHRMS.BLL.Services.Implementation
+{
+    public class AIService : IAIService
+    {
+        private readonly IAIRepo AIrepo;
+        public AIService(IAIRepo AIrepo)
+        {
+            this.AIrepo = AIrepo;
+        }
+        public async Task<(bool, string?, AIVM)> GetAnswerAsync(string prompt)
+        {
+            var answer = await AIrepo.AskAsync(prompt);
+            if (string.IsNullOrEmpty(answer))
+            {
+                return (false, "No answer found", new AIVM
+                {
+                    Question = prompt,
+                    Answer = "I'm sorry, I couldn't find an answer to your question."
+                });
+            }
+            return (true, null, new AIVM { Question = prompt, Answer = answer });
+        }
+    }
+}
